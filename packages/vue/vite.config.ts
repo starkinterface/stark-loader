e0fe1e26c9b3ceea77b03fb14149a6952cf25bdf
enjidev <me@enji.dev>
2026-03-19 01:36:22 +0700

    style: make relative imports at the same level as internal

--- packages/vue/vite.config.ts
+++ packages/vue/vite.config.ts
@@ -1,9 +1,9 @@
 import { resolve } from "node:path"
+import pkg from "./package.json" with { type: "json" }
 import vue from "@vitejs/plugin-vue"
 import vueJsx from "@vitejs/plugin-vue-jsx"
 import { defineConfig } from "vite"
 import dts from "vite-plugin-dts"
-import pkg from "./package.json" with { type: "json" }
 
 const root = import.meta.dirname
 
